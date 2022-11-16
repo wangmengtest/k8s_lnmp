48c7405bac640286426654fce8d5dbdf6db90dfa
553168431@qq.com <553168431@qq.com>
2022-11-16 12:31:02 +0800

    11

---
+++ 2020/docker/k8s_lnmp/local_storage/wordpress/index.php
@@ -0,0 +1,16 @@
+<?php
+echo 123;
+$con = mysqli_connect(getenv('WORDPRESS_DB_HOST'), "root", getenv('WORDPRESS_DB_PASSWORD'), "mysql");
+if (mysqli_connect_errno()) {
+    printf("connection failed: %s\n", mysqli_connect_error());
+    exit();
+}
+$query = "SELECT VERSION()";
+$res = mysqli_query($con, $query);
+if ($res) {
+    $row = mysqli_fetch_row($res);
+    echo $row[0];
+}
+mysqli_free_result($res);
+mysqli_close($con);
+phpinfo();
